5f6d0b817ff9a8ad1fc816f68742b61fb002e417
pg <piergiorgio.palla@ift.cnr.it>
2014-03-03 11:50:35 +0100

    Set up repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fequi::Application.config.secret_token = '7ff1a913332bc8977ae58c6798ce37634b383c58adad2eba70747bf52cb82c0345cd95486fff72d553b99834cdd127b665cd7e04dffea6f44968eef7746e66c6'
